fd7899a94995bb92aecb3768494b2e2b6952e1b0
bartas93 <jablonski.bartosz93@gmail.com>
2018-05-09 14:59:49 +0200

    Updated module.ts and test connection with api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from './services/user.service';
+import { UserTo } from './models/UserTo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private users: UserTo[];
 
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.userService.getAllUsers().subscribe((data) => {
+      this.users = data;
+    }, err => {
+
+    })
+  }
 
 }
 
